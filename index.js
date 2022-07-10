f217e1fdc74d89df5f00ed2265102fb29f50f7aa
Tulio <tuliohcosta@gmail.com>
2022-07-09 22:10:39 -0300

    refactor: remove unused code

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-const path = require('path')
 const express = require('express')
 
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'dist')))
 app.set('port', process.env.PORT || 3000)
 
 app.get('/', function (req, res) {
